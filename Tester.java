bce84843a4b5d923970693a4ea721efccfee5994
Juan <juanignacios.2000@gmail.com>
2020-09-15 15:59:41 -0300

    Agrego la clase Alumno y Tester(modificada) al repositorio local Proyecto-1-Parte-1

--- Tester.java
+++ Tester.java
@@ -2,7 +2,7 @@ package GIT;
 
 public class Tester {
 	public static void main(String[] args) {
-		Alumno a=new Alumno("Juan","Sanchez",125557);
+		Alumno a=new Alumno("Juan","Sanchez",125755);
 		System.out.println("Alumno: "+a.getNombre()+" "+a.getApellido()+". LU: "+a.getLU());
 	}
 }

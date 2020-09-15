cd5941346c9b83e27c336dfdd175efdedefb85a8
Juan <juanignacios.2000@gmail.com>
2020-09-15 17:10:54 -0300

    Guardo la clase Alumno en el repositorio local GIT, luego de eliminar la clase Tester

--- Tester.java
+++
@@ -1,8 +0,0 @@
-package GIT;
-
-public class Tester {
-	public static void main(String[] args) {
-		Alumno a=new Alumno("Juan","Sanchez",155557);
-		System.out.println("Alumno: "+a.getNombre()+" "+a.getApellido()+". LU: "+a.getLU());
-	}
-}
